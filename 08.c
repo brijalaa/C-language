fd0f11b5b79112359104ba631fd072fea997ef83
brijalaa <brijalaapandey@gmail.com>
2024-12-07 20:49:05 +0545

    Add files via upload

---
+++ 08.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    printf("a=");
+    scanf("%f", &a);
+    printf("%f", 97%a);
+    return 0;
+    
+ 
+}
---
+++ 09.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int x=2, y=3, z=3, k=1;
+    float g=3*x/y-z+k;
+    printf("the valuse of g is %d", g);
+    return 0;
+
+}
