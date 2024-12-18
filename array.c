ee0c541ac1136cf69ac850fccd747478911b33a8
Brijala <brijalaapandey@gmail.com>
2024-12-18 22:10:26 +0545

    .

---
+++ array.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+ int marks[5];
+ printf("Enter marks of 5 students");
+ for (int i = 0; i < 5; i++)
+ {
+    scanf("%d", &marks[i]);
+ }
+ printf("marks 1,2,3,4,5  is %d,%d,%d,%d,%d,", marks[0],marks[1],marks[2],marks[3],marks[4]);
+return 0;
+}
---
+++ array2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+   int marks[]={45,52,333,24,85};
+   for (int i = 0; i < 5; i++)
+   {
+    printf("the marks at index %d is %d\n", i, marks[i]);
+   }
+   
+return 0;
+}
